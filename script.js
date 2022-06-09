7e260f47acf050033f3f7dc708cfade532f3e160
Carina Cunha <carina.cunha13@gmail.com>
2022-06-08 23:03:49 -0300

    Requisito 21

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// First event reaction
 const buttonSend = document.getElementById('button');
 
 function alertMessage() {
@@ -12,6 +13,7 @@ function alertMessage() {
 }
 buttonSend.addEventListener('click', alertMessage);
 
+// Second event reaction
 const buttonSubmit = document.getElementById('submit-btn');
 const checkInfo = document.getElementById('agreement');
 function enable() {
@@ -20,8 +22,36 @@ function enable() {
 
 checkInfo.addEventListener('change', enable);
 
+// Third event reaction
 const textbox = document.getElementById('textarea');
 const counter = document.getElementById('counter');
 textbox.onkeyup = (e) => {
   counter.innerHTML = `${500 - e.target.value.length} caracteres restantes.`;
 };
+
+// Get elements
+const originalForm = document.getElementById('evaluation-form');
+const finalForm = document.getElementById('form-data');
+const family = [...document.getElementsByClassName('family')];
+const evaluation = [...document.getElementsByClassName('eval')];
+const subjects = [...document.getElementsByClassName('subject')];
+
+// eslint-disable-next-line max-lines-per-function
+buttonSubmit.addEventListener('click', (event) => {
+  const resultFamily = family.find((element) => element.checked).value;
+  const resultEval = evaluation.find((element) => element.checked).value;
+  const selectedSubjects = subjects.filter((element) => element.checked);
+  const resultSubjects = selectedSubjects.map((element) => element.value).join(', ');
+
+  finalForm.innerText = `Nome: ${document.getElementById('input-name').value} \
+  ${document.getElementById('input-lastname').value}\n
+  Email: ${document.getElementById('input-email').value}\n
+  Casa: ${document.getElementById('house').value}\n
+  Família: ${resultFamily}\n
+  Matérias: ${resultSubjects}\n
+  Avaliação: ${resultEval}\n
+  Observações: ${document.getElementById('textarea').value}`;
+
+  originalForm.style.display = 'none';
+  event.preventDefault();
+});
